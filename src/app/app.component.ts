c4b13d69750852a4c4d62374141f5026826a3c75
Artem Sarana <registrr2009@yandex.ru>
2021-03-08 11:42:04 +0300

    Добавлены координаты поля по умолчанию

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -15,7 +15,7 @@ export class AppComponent implements OnInit {
   progress$: Observable<Progress> = this.backendService.progress;
 
   selectedDate = '';
-  polygonCoords = '';
+  polygonCoords = 'POLYGON((39.2376 47.3518,39.2514 47.2470,39.2156 47.2302,39.2349 47.04439,39.3214 47.0442, 39.3145 46.9483,39.4831 46.9851,39.5356 47.0640,39.74921 47.0819,39.7705 47.0078,40.0286 46.9727, 39.9461 47.3648,39.2376 47.3518))';
 
   constructor(private backendService: BackendService) {
   }
